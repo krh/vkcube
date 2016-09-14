ea6fe71417d919e5c3d8894e77b44cb225a97948
Philipp Zabel <philipp.zabel@gmail.com>
2016-09-14 22:31:00 +0200

    Query physical device surface capabilities before creating swap chain

--- main.c
+++ main.c
@@ -644,6 +644,10 @@ init_xcb(struct vkcube *vc)
 static void
 alloc_buffers_xcb(struct vkcube *vc)
 {
+   VkSurfaceCapabilitiesKHR surface_caps;
+   vkGetPhysicalDeviceSurfaceCapabilitiesKHR(vc->physical_device, vc->surface,
+                                             &surface_caps);
+
    VkBool32 supported;
    vkGetPhysicalDeviceSurfaceSupportKHR(vc->physical_device, 0, vc->surface,
                                         &supported);
@@ -914,6 +918,10 @@ init_wayland(struct vkcube *vc)
    }
 
    assert(format != VK_FORMAT_UNDEFINED);
+
+   VkSurfaceCapabilitiesKHR surface_caps;
+   vkGetPhysicalDeviceSurfaceCapabilitiesKHR(vc->physical_device, wsi_surface,
+                                             &surface_caps);
 
    VkBool32 supported;
    vkGetPhysicalDeviceSurfaceSupportKHR(vc->physical_device, 0, wsi_surface,
