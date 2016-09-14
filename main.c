1b93858cbf3ea2875f0bff61ea63bac19af827bb
Philipp Zabel <philipp.zabel@gmail.com>
2016-09-14 21:04:46 +0200

    Fix the mipmap level count when initializing buffers

--- main.c
+++ main.c
@@ -211,7 +211,7 @@ init_buffer(struct vkcube *vc, struct vkcube_buffer *b)
                         .subresourceRange = {
                            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
                            .baseMipLevel = 0,
-                           .levelCount = 0,
+                           .levelCount = 1,
                            .baseArrayLayer = 0,
                            .layerCount = 1,
                         },
